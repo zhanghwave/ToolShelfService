cebaf3696e813a2c3ce5154c3b803935625aa273
zhanghwave <2446734662@qq.com>
2020-07-19 13:02:47 +0800

    add biji

---
+++ git 笔记.java
@@ -0,0 +1,99 @@
+﻿工作区：
+就是你在电脑上看到的目录，比如目录下testgit里的文件(.git隐藏目录版本库除外)。或者以后需要再新建的目录文件等等都属于工作区范畴。
+版本库(Repository)：
+工作区有一个隐藏目录.git,这个不属于工作区，这是版本库。其中版本库里面存了很多东西，其中最重要的就是stage(暂存区)，还有Git为我们自动创建了第一个分支master,以及指向master的一个指针HEAD。
+
+我们前面说过使用Git提交文件到版本库有两步：
+第一步：是使用 git add 把文件添加进去，实际上就是把文件添加到暂存区。
+第二步：使用git commit提交更改，实际上就是把暂存区的所有内容提交到当前分支上。
+
+git配置：
+git config --global user.name "zhanghwave";
+git config --global iser.email "2446734662@qq.com"
+
+查看配置：
+git config --global --list   
+git config --global -l
+
+查看帮助：
+git --help
+git help [command]
+
+生成ssh.key:
+ssh-keygen -t rsa -C "2446734662@qq.com"
+
+提交代码：
+git clone [url] ==> vi [file] ==> git add [file] / git add .  ==> git commit -m "first commit"  ==> git push 
+
+查看所有分支：
+git branch -a
+
+查看当前分支：
+git branch
+
+创建并切换分支：
+git checkout -b dev
+
+切换分支：
+git checkout [branch name]
+
+commit前查看状态：
+git status
+
+查看commit记录：
+git log  / git log –pretty=oneline
+
+向前回退：
+git reset --hard head^
+git reset --hard head^^
+git reset --hard head~100
+
+恢复回退前的某一次commit：
+git reset --hard [commit 版本号]
+
+查找commit版本号：
+git reflog
+
+修改了文件 但没有add 发现改错了，恢复之前的(在工作区做的修改全部撤销)：
+git checkout -- [file]
+
+提交代码到远程：
+      第一次: git push -u origin master     由于远程库是空的，我们第一次推送master分支时，加上了–u参数，Git不但会把本地的master分支内容推送的远程新的master分支，还会把本地的master分支和远程的master分支关联起来，在以后的推送或者拉取时就可以简化命令。
+
+本地的分支和远程分支建立联系:
+git push --set-upstream origin master
+git 记住用户密码 只针对本仓库：
+git config  credential.helper store
+
+合并dev到master分支：
+git checkout master
+git merge dev
+
+删除分支：
+git branch -d dev
+git push origin --delete dev
+
+查看远程库信息：
+git remote -v
+
+冲突分析：
+A B同时下载代码 
+1. A删除文件test7并提交代码，B执行git pull， B代码里没有了test7，其他相同。
+2. A增加文件test7并提交代码，B执执行git add test8 和 git pull， B代码里有test7 test8
+3. B添加test8 修改test5，加一行内容，git push, A git pull ,A代码里有test8，test5增加了内容
+4. A修改test5 减去一行内容，修改另一行内容，git push ，B git pull 和A的test5 相同
+
+pull冲突：
+强制先放弃本地修改后更新，再pull：
+git fetch --all
+git reset --hard origin/dev
+git pull
+
+代码覆盖问题：
+先存；再pull；然后合并本地修改的内容和远程pull下来的内容，解决冲突，决定到底需要哪些内容；最后重新走提交流程。
+git stash
+git pull
+git stash pop
+git add .
+git commit -m "resolve conflict"
+git push
---
+++ mybatis 笔记.java
@@ -0,0 +1,151 @@
+﻿mybatis 
+
+注解@Param
+当你使用了使用@Param注解来声明参数时，如果使用 #{} 或 ${} 的方式都可以,当你不使用@Param注解来声明参数时，必须使用使用 #{}方式。如果使用 ${} 的方式，会报错。
+不使用@Param注解时，最好传递 Javabean。在SQL语句里就可以直接引用JavaBean的属性，而且只能引用JavaBean存在的属性。
+1. 获取参数：
+
+   第一种，mapper interface 未使用注解 @Param
+    (1)单参
+	
+	mybatis中内置了 ==_parameter== 参数来代替参数名
+    <select id="batSelect" parameterType="java.lang.String" resultMap="BaseResultMap">
+        select * from works1 WHERE id in (${_parameter})
+    </select>
+	
+	<select id="batSelect" parameterType="com.company.project.model.Works1" resultMap="BaseResultMap">
+        select * from works1 WHERE id in = #{id}   
+    </select>
+	
+	(2)多参数
+	
+	public User getUserByNameAndPass(String name,String pass);  
+	<select id="getUserByNameAndPass"  resultMap="BaseResultMap" parameterType="Object">
+		SELECT * FROM t_user  WHERE  t_name = #{0} and t_pass = #{1}
+	</select>
+	注释：#{0} 低版本mybatis 
+	
+	(3)List 或 Array集合参数
+	
+	public int batchDelUser(List<String> uuidList);
+	<delete id="batchDelUser" parameterType="java.util.List">
+		DELETE FROM t_user WHERE uuid IN
+		<foreach collection="list" index="index" item="uuid" open="(" separator="," close=")">
+			#{uuid}
+		</foreach>
+	</delete>
+	
+	<delete id="batchDelUser" parameterType="java.util.List">
+		DELETE FROM t_user WHERE uuid IN
+		<foreach collection="array" index="index" item="uuid" open="(" separator="," close=")">
+			#{uuid}
+		</foreach>
+	</delete>
+	
+	(4) 对象
+	
+	public int updateUser(User user);   
+	<select id="updateUser"  resultMap="BaseResultMap" parameterType="Object">
+		update t_user set t_name = #{name}, t_pass = #{pass} where uuid=#{uuid}
+	</select>	
+	
+	第二种，使用@Param注解
+	
+	(1)单参
+	
+	public interface Works1Mapper extends Mapper<Works1> {
+        List<Works1> batSelect(@Param("abc")String ids);
+    }
+	<select id="batSelect" parameterType="java.lang.String" resultMap="BaseResultMap">
+      //都不会报错
+        select * from works1 WHERE id in (#{abc}) 
+        select * from works1 WHERE id in (${abc})  
+    </select>
+	
+	(2)多参数
+	
+	public User getUserByNameAndPass(@Param("name") String name, @Param("pass")String pass);  
+	<select id="getUserByNameAndPass"  resultMap="BaseResultMap" parameterType="Object">
+		SELECT * FROM t_user  WHERE  t_name = #{param1} and t_pass = #{param2}
+		SELECT * FROM t_user  WHERE  t_name = #{name} and t_pass = #{pass}
+		SELECT * FROM t_user  WHERE  t_name = '${name}' and t_pass = ${pass}
+	</select>
+
+    (3)获取对象中的值
+	
+     直接使用对象内的属性取值，如Works1对象中有个id属性.这时候#{}和${}都不会报错
+	
+	(3)Map参数
+	
+	使用@Param注解,可用自定义变量
+	public User getUserByMap(Map<String,Object> map);
+	<select id="getUserByMap"  resultMap="BaseResultMap" parameterType="java.util.Map">
+		SELECT * FROM t_user  WHERE  t_name = #{name} and t_pass = #{pass}
+	</select>
+	
+	(4)实体对象参数
+	
+	public interface Works1Mapper extends Mapper<Works1> {
+		List<Works1> QueryAllByBean(@Param("wk") Works1 works);
+    }
+	<select id="QueryAllByBean"  resultMap="BaseResultMap">
+        select * from works1 where id = #{wk.id};
+    </select>
+	
+	//JAVA 代码
+	public List<Group> getUserRoleRelByUserUuid(@Param("groupUuid") String userUuid,@Param("roleList")List<String> roleUuidList);
+	//SQL
+	SELECT * from user_role where groupUuid=#{groupUuid}
+		<choose>
+			<when test="roleList!=null&amp;&amp;roleList.size()&gt;0">
+				AND roleUuid IN
+				<foreach collection="roleList" index="index" item="roleUuid" open="(" separator="," close=")">
+					#{roleUuid}
+				</foreach>
+			</when>
+			<otherwise>
+				AND roleUuid IN ('')
+			</otherwise>
+		</choose>
+	
+2. 用#{}取值时会默认加上双引号 ''
+    (1) 区别：
+    首先清楚一点，动态 SQL 是 mybatis 的强大特性之一，在 mapper 中定义的参数传到 xml 中之后，在查询之前 mybatis 会对其进行动态解析，#{} 和 ${} 在预编译中的处理是不一样的：
+    例如：select * from t_user where userName = #{name};
+    #{}预编译：用一个占位符 ? 代替参数:select * from t_user where userName = ?
+    #{}预编译：会将参数值一起进行编译：select * from t_user where userName = 'zhangsan'
+    (2) 使用场景：
+    一般情况首选#{}，因为这样能避免sql注入；如果需要传参 动态表名、动态字段名时，需要使用${}
+    比如：select * from ${tableName} where id > #{id};
+    (3) SQL注入问题：
+    举个例子，如果使用${}出现的注入问题：
+    select * from ${tableName};
+    如果传参 t_user;delete from t_user，则预编译后的sql如下，将会导致系统不可用：
+    select * from t_user;delete from t_user;
+    (4) like 语句防注入：
+    使用concat函数：
+    select * from t_user where name like concat('%', #{name}, '%')
+
+3. mysql字段类型转换
+	mysql会自动转换：当你类型为int 但传入的值为varchar 他会把前几个'数字挑出来'转换为int 类型来进行查询
+	
+	如果你的 aid = 1  你查询语句是SELECT * FROM 表名 WHERE aid='1dasd';
+	在编译的时候，编译器会把  1   挑出来进行转换，在进行查询，
+		
+	如果你的 aid = 1     你查询语句是SELECT * FROM 表名 WHERE aid='dasd';  
+	第一个不为数字，所以无法转换，查询出来的结果就为空(不会报错)，     
+
+	如果你的 aid = 1     你查询语句是SELECT * FROM 表名 WHERE aid='123da';
+	在编译的时候，编译器会把  123   挑出来进行转换，在进行查询，但查询结果为空（找不到这个对应的id）；
+
+	如果你的 aid = 1     你查询语句是SELECT * FROM 表名 WHERE aid='da1sd';
+	查询结果为空，因为编译期是从第一个开始查看是否为数字，如果不是不会往后查；
+		
+4. 模糊查询concat:
+	SELECT * FROM tableName WHERE name LIKE CONCAT(CONCAT('%', #{text}), '%');
+	<select id="getMaxSerialCode" resultType="java.lang.String"  parameterType="Object">
+        SELECT count(*) FROM
+        itil_publish_order
+        WHERE serial_code LIKE CONCAT('%',#{codeStr},'%')
+        ORDER BY serial_code DESC LIMIT 1
+	</select>
