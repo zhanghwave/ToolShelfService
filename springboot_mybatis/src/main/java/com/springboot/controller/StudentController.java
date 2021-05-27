2d381c5fb5a5d6568fb990bcc1fa61e0074d7e39
zhanghww <zhanghww@dcits.com>
2021-05-27 12:19:17 +0800

    update files

--- springboot_mybatis/src/main/java/com/springboot/controller/StudentController.java
+++ springboot_mybatis/src/main/java/com/springboot/controller/StudentController.java
@@ -5,6 +5,8 @@ import com.springboot.pojo.StudentExample;
 import com.springboot.service.StudentService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,21 +17,24 @@ import java.util.List;
  * @author: zhanghww
  * @create: 2021-05-04 10:32
  **/
-@RestController
+@Controller
 @RequestMapping(value = "/student")
 public class StudentController {
 
     Logger logger = Logger.getLogger(StudentController.class);
+    private static final String INDEX_PAGE = "index";
 
     @Autowired
     StudentService studentService;
 
     @GetMapping(value = "/getStudent/{id}")
+    @ResponseBody
     public Student getStudentById(@PathVariable String id) {
         return studentService.getStudentById(id);
     }
 
    @RequestMapping(value = "/listStudent", method = RequestMethod.POST)
+   @ResponseBody
    public List<Student> studentList(@RequestBody Student student) {
        StudentExample studentExample = new StudentExample();
        StudentExample.Criteria criteria = studentExample.createCriteria();
@@ -42,7 +47,25 @@ public class StudentController {
    }
 
     @GetMapping(value = "/test")
+    @ResponseBody
     public String test(@RequestParam(value = "sname", required = false) String name) {
         return "hello!";
+    }
+
+    /**
+    * @Description: thymeleaf 测试
+    * @Param: [map]
+    * @return: java.lang.String
+    * @Author: zhanghww
+    * @Date: 2021/5/17
+    */
+    @RequestMapping(value = "/thymeleaf", method = RequestMethod.GET)
+    public String thymeleafTest(@RequestParam String sno, Model model){
+        StudentExample studentExample = new StudentExample();
+        StudentExample.Criteria criteria = studentExample.createCriteria();
+        criteria.andSnoGreaterThan(sno);
+        List<Student> students = studentService.selectByExample(studentExample);
+        model.addAttribute("students", students);
+        return INDEX_PAGE;
     }
 }
